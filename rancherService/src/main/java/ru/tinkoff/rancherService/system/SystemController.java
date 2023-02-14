3caeca7a95a0ffb4259e7b5680869cda9bc1654a
rishat <sagidullinrishat@mail.ru>
2023-02-14 19:08:45 +0500

    Добавлена функция для проверки работы сервисов.

---
+++ rancherService/src/main/java/ru/tinkoff/rancherService/system/SystemController.java
@@ -0,0 +1,19 @@
+package ru.tinkoff.rancherService.system;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system")
+@NoArgsConstructor
+public class SystemController {
+
+
+    @GetMapping("/liveness")
+    public HttpStatus isAlive(){
+        return HttpStatus.OK;
+    }
+}
