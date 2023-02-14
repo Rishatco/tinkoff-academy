2058613937250e5a4cedb9ab203e57fde5b5a6ed
rishat <sagidullinrishat@mail.ru>
2023-02-14 12:47:13 +0500

    Инициализировал все сервисы.

---
+++ landscapeService/src/test/java/ru/tinkoff/landscapeService/LandscapeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.landscapeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandscapeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
